a75e4a3689964e2f052461b82d66dec17125361b
Noah Emke <noahremke@gmail.com>
2022-09-19 18:59:00 -0400

    Added content type negotiation and associated helper functions as well as ran and passed tests.

--- src/server.js
+++ src/server.js
@@ -1,33 +1,94 @@
-//#region Imports
+// #region Imports
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
-//#endregion
+// #endregion
 
 // Read and parse settings file
-const settings = JSON.parse(fs.readFileSync(__dirname + '/../settings.json'));
+const settings = JSON.parse(fs.readFileSync(`${__dirname}/../settings.json`));
 
-//#region Settings
+// #region Settings
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
 // Attempt to verify and set the webdir to serve from
 let webdir;
 try {
-  webdir = fs.realpathSync(__dirname + '/../'
-    + (process.env.NODE_WEBDIR || settings.webdir));
+  webdir = fs.realpathSync(`${__dirname}/../${process.env.NODE_WEBDIR || settings.webdir}`);
 } catch (error) {
-  if (error.code = 'NOENT') {
-    console.log(`ERROR: webdir directory ${error.path} not found. ` +
-      "Check the webdir is set correctly in settings.json");
+  // Log error and exit if web directory could not be established
+  if (error.code === 'NOENT') {
+    console.log(`ERROR: webdir directory ${error.path} not found. `
+      + 'Check the webdir is set correctly in settings.json');
   } else {
     console.log(error);
   }
   process.exit(1);
 }
-//#endregion
+// #endregion
 
+// Struct to manage any cases that should be handled in a specific way
 const specialCases = {
-  '/': () => console.log("/ reached"), //serveFile(webdir + '/client.html'),
+  '/': () => console.log('/ reached'), // serveFile(webdir + '/client.html'),
 };
+
+// Helper to check if a file is accessable TODO: move to another file
+function checkValidFile(filePath) {
+  try {
+    fs.accessSync(filePath);
+  } catch (error) {
+    return false;
+  }
+  return true;
+}
+
+// Determines the appropriate content type to return based on the
+// clients accepted types and the types available for this endpoint. TODO: move to other file
+function determineType(acceptedTypes, availableTypes) {
+  // Search the clients accepted types for the first option available.
+  const type = acceptedTypes.find((element) => {
+    // Get rid of version or quality values and presume the list is already
+    // sorted by preference.
+    const testedType = element.split(';')[0];
+
+    // Check if the type in question is available
+    if (availableTypes.includes(testedType)) {
+      return true;
+    }
+
+    // Check if the type in question is a wildcard
+    if (testedType === '*/*') {
+      return true;
+    }
+
+    return false;
+  });
+
+  // Get rid of version or quality values
+  const trimmedType = type.split(';')[0];
+
+  if (trimmedType === '*/*') {
+    // If the client doesnt care, use default
+    return acceptedTypes[0];
+  }
+
+  if (trimmedType) {
+    // If a type was found, use it
+    return trimmedType;
+  }
+
+  // Indicate that there are no acceptable responses.
+  return false;
+}
+
+function jsonToXml(json) {
+  let xml = '<response>';
+  Object.keys(json).forEach((key) => {
+    xml += `<${key}>${json[key]}</${key}>`;
+  });
+  xml += '</response>';
+
+  return xml;
+}
 
 function onRequest(request, response) {
   const parsedUrl = url.parse(request.url);
@@ -39,27 +100,33 @@ function onRequest(request, response) {
   if (specialCases[resolvedPath]) {
     specialCases[resolvedPath](request, response, parsedUrl);
   } else if ((request.method === 'GET' || request.method === 'HEAD') && checkValidFile(webdir + resolvedPath)) {
-    console.log("serving " + webdir + resolvedPath); //serveFile(webdir + resolvedPath, request, response);
-  } else {
+    //
+    console.log(`serving ${webdir}${resolvedPath}`); // serveFile(webdir + resolvedPath, request, response);
+  } else { // TODO Break out to a funuction to send a response.
     response.statusCode = 404;
-    
-    
-    //response.setHeader()
+    const contentType = determineType(acceptedTypes, ['application/json', 'application/xml']);
+    response.setHeader('Content-Type', contentType);
+
+    if (request.method !== 'HEAD') {
+      const responseJSON = {
+        id: '404NotFound',
+        message: 'The page or resource you have requested does not exist.',
+      };
+
+      let responseContent;
+      if (contentType === 'application/xml') {
+        responseContent = jsonToXml(responseJSON);
+      } else {
+        responseContent = JSON.stringify(responseJSON);
+      }
+
+      response.write(responseContent);
+    }
+
+    response.end();
   }
 }
 
-http.createServer(onRequest).listen(port, () =>
-  console.log(`Serving files in ${webdir} on port ${port}`));
-
-function determineType(acceptedTypes, availableTypes) {
-  const type = acceptedTypes.indexOf(acceptedTypes.find());
-}
-
-function checkValidFile(filePath) {
-  try {
-    fs.accessSync(filePath)
-  } catch (error) {
-    return false;
-  }
-  return true;
-}
+http.createServer(onRequest).listen(port, () => {
+  console.log(`Serving files in ${webdir} on port ${port}`);
+});
