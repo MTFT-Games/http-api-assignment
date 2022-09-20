35e32548052d69399a26bbde46e67ba78c005e2d
Noah Emke <noahremke@gmail.com>
2022-09-20 17:56:24 -0400

    Fuck i forgot a file

---
+++ src/utilities.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+
+// Helper to check if a file is accessible.
+function checkValidFile(filePath) {
+  try {
+    fs.accessSync(filePath);
+  } catch (error) {
+    return false;
+  }
+  return true;
+}
+
+// Determines the appropriate content type to return based on the
+// clients accepted types and the types available for this endpoint.
+function determineType(acceptedTypes, availableTypes) {
+  // Search the clients accepted types for the first option available.
+  const type = acceptedTypes.find((element) => {
+    // Get rid of version or quality values and presume the list is already
+    // sorted by preference.
+    const testedType = element.split(';')[0];
+
+    // Check if the type in question is available
+    if (availableTypes.includes(testedType)) {
+      return true;
+    }
+
+    // Check if the type in question is a wildcard
+    if (testedType === '*/*') {
+      return true;
+    }
+
+    return false;
+  });
+
+  // Get rid of version or quality values again
+  const trimmedType = type.split(';')[0];
+
+  if (trimmedType === '*/*') {
+    // If the client doesnt care, use default
+    return acceptedTypes[0];
+  }
+
+  if (trimmedType) {
+    // If a type was found, use it
+    return trimmedType;
+  }
+
+  // Indicate that there are no acceptable responses.
+  return false;
+}
+
+module.exports = {checkValidFile, determineType,};
